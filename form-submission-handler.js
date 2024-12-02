faf129673169ce83d2f9979e127fe7b8389eba7b
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2024-12-02 10:19:54 -0800

    made it more clear that a vote is sent

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -75,7 +75,7 @@
       xhr.onreadystatechange = function() {
           if (xhr.readyState === 4 && xhr.status === 200) {
             sendBtn.disabled = false;
-            sendBtn.textContent = "THX!";
+            sendBtn.textContent = "Sent! THX!";
           }
       };
       // url encode form data for sending as post data
