13637d30dce3951419e3c10c5a71b36d0a7d0c6f
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2025-10-03 16:33:49 -0700

    category name change

--- voting/categories.js
+++ voting/categories.js
@@ -1,13 +1,14 @@
 const categories = [
-    ["Best Comedy","Description would go here. It would say something about this category."],
-    ["Best Fluff","test2"],
-    ["Best Hurt/Comfort","test3"],
-    ["Best Angst","test4"],
+    ["Best Use of Comedy","Description would go here. It would say something about this category."],
+    ["Best Use of Fluff","test2"],
+    ["Best Use of Hurt/Comfort","test3"],
+    ["Best Use of Angst","test4"],
+    ["Best Use of Romance","test"],
     ["Best Action","test"],
-    ["Best Adventure","test"],
-    ["Best Plot","test"],
     ["Best Characters","test"],
+    ["Best Plot","test"],
     ["Best World-Building","test"],
     ["Best Atmosphere","test"],
+    ["Best Prose","test"]
     ["Best Story","test"]
 ];
