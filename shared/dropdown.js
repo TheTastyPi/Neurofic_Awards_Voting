d510fa1d697b3fa381f83f0aec2ae2cec9bd3630
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2025-11-04 20:17:20 -0800

    dropdown refactor

--- shared/dropdown.js
+++ shared/dropdown.js
@@ -1,11 +1,35 @@
 const dropdown = document.getElementsByClassName("dropdown")[0];
-const dropdownContent = document.getElementsByClassName("dropdownContent")[0];
+const startYear = 2024;
+const yearsElapsed = 1;
 
-dropdown.addEventListener("click",function(e){
-    dropdown.classList.toggle("active");
-    if (dropdownContent.style.display == "") {
-        dropdownContent.style.display = "block";
-    } else {
-        dropdownContent.style.display = "";
+function initDropdownContent() {
+    let content = document.createElement("div");
+    content.classList.add("dropdownContent");
+    for (let i = startYear; i < startYear + yearsElapsed; i++) {
+        let link = document.createElement("a");
+        link.href = `/archive/${i}/`;
+        link.textContent = i;
+        content.appendChild(link);
     }
-});
+    dropdown.appendChild(content);
+    
+    dropdown.addEventListener("click",function(e){
+        dropdown.classList.toggle("active");
+        if (content.style.display == "") {
+            content.style.display = "block";
+        } else {
+            content.style.display = "";
+        }
+    });
+}
+
+function initDropdown() {
+    let label = document.createElement("button");
+    label.classList.add("dropdownLabel");
+    label.textContent = "Past Results";
+    dropdown.appendChild(label);
+
+    initDropdownContent()
+}
+
+initDropdown();
