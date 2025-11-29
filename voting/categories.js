503000ebcf12f1a9e5745d0e68465cd36c7713c4
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2025-11-28 16:11:59 -0800

    new rules and award desc

--- voting/categories.js
+++ voting/categories.js
@@ -1,14 +1,14 @@
 const categories = [
-    ["Best Use of Comedy","Description would go here. It would say something about this category."],
-    ["Best Use of Fluff","test2"],
-    ["Best Use of Hurt/Comfort","test3"],
-    ["Best Use of Angst","test4"],
-    ["Best Use of Romance","test"],
-    ["Best Action","test"],
-    ["Best Characters","test"],
-    ["Best Plot","test"],
-    ["Best World-Building","test"],
-    ["Best Atmosphere","test"],
-    ["Best Prose","test"],
-    ["Best Story","test"]
+    ["Best Use of Comedy","The funniest stories. This category is for ones that made you laugh, chuckle, or you otherwise found highly amusing."],
+    ["Best Use of Fluff","Stories that left you feeling warm and fuzzy. This category is for works that are pleasant, hopeful, and generally positive."],
+    ["Best Use of Hurt/Comfort","There's nothing that highlights positivity more than negativity. This category is for fics that, while sometimes angsty and heavy, also contain happier moments too."],
+    ["Best Use of Angst","Sadness, anger, misery, despair. If the story's overall theme is negative it falls into the angst category."],
+    ["Best Use of Romance","This category is for stories that heavily feature love that goes beyond family and friendship."],
+    ["Best Action","Lights, camera, action! These stories feature motion and quick paced scenes, never leaving a dull moment."],
+    ["Best Characters","The Neuroverse has a lot of amazing characters, and this category highlights the ones written masterfully."],
+    ["Best Plot","There are many parts to a story and plot has to be one of the most important components. This category is for the best overall intrigue and storyline."],
+    ["Best World-Building","Some stories go above and beyond and truly develop what feels like an entire other world. This category captures that feeling of more to the story's time, place, or environment."],
+    ["Best Atmosphere","If an underlying emotion is well evoked through the story's development, you've got yourself a great atmosphere. This category captures works that leave you with lingering— happy or sad— emotions afterward."],
+    ["Best Prose","Some stories have a particular cadence that simply flows. This category features fics that contain a strong, consistent, and well paced structure."],
+    ["Best Story","The story you felt was the best. Simply as that."]
 ];
